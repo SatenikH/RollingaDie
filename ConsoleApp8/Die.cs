66b3f1560c3256cb5a25af19b64ae41b743d32bd
SatyHar <hhar.satenik@gmail.com>
2019-04-17 17:52:21 +0400

    adding files

---
+++ ConsoleApp8/Die.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RollingDie
+{
+    /// <summary>
+    /// Rolling die description
+    /// </summary>
+    internal class Die
+    {
+        public delegate void Action();
+        public event Action OnFourCount;
+        public event Action OnLessEquals;
+
+        private int value = 1;
+        private int sides = 6;
+        public Die(int sides)
+        {
+            this.sides = sides;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+        public int getSides()
+        {
+            return sides;
+        }
+        public int Roll()
+        {
+            Random rand = new Random();
+            value = rand.Next(1, sides+1);
+            return value;
+        }
+        public void TestOfRolling(int count)
+        {
+            int[] diesArr = new int[50];
+            for (int i = 0; i < count; i++)
+            {
+                diesArr[i] = Roll();
+                Console.WriteLine(diesArr[i]);
+                if (i >= 1 && diesArr[i] == 4 && diesArr[i - 1] == 4)
+                {
+                    OnFourCount();
+                }
+
+                if (i >= 4 && diesArr[i] + diesArr[i - 1] + diesArr[i - 2] + diesArr[i - 3] + diesArr[i - 4] >= 20)
+                {
+                    OnLessEquals();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RollingDie
+{
+    internal class Program
+    {
+        public static void FourTwoHeandler()
+        {
+            Console.WriteLine("Two fours in a row");
+        }
+        public static void GreatEqualsHeandler()
+        {
+            Console.WriteLine("Great or equal 20");
+        }
+        static void Main(string[] args)
+        {
+            Die die = new Die(6);
+            die.OnFourCount += FourTwoHeandler;
+            die.OnLessEquals += GreatEqualsHeandler;
+
+            die.TestOfRolling(50);
+
+            Console.ReadLine();
+        }
+    }
+}
